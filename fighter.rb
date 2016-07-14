4c34f2187582a1c9ea342c31e15a853a6434b3e0
crashtestzombie0@gmail.com <crashtestzombie0@gmail.com>
2016-07-14 09:32:10 -0400

    initial commit

---
+++ fighter.rb
@@ -0,0 +1,23 @@
+class Fighter
+  attr_reader :name
+  attr_accessor(:health, :power)
+  def initialize(name, health, power)
+    @name = name
+    @health = health
+    @power = power
+  end
+  def attack(enemy)
+    enemy.lose_health(self.power, enemy.health)
+  end
+  def lose_health(enemy_power, health)
+    self.health -= enemy_power
+  end
+end
+
+goku = Fighter.new("Goku", 9000, 120)
+pikachu = Fighter.new("Pikachu", 8000, 60)
+
+puts goku.attack(pikachu)
+puts pikachu.inspect
+puts pikachu.attack(goku)
+puts goku.inspect
